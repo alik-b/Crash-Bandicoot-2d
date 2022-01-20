34580740aaaf83d5efffb89da526e7ae33af34ec
alikb <alikb@uw.edu>
2022-01-20 13:44:50 -0800

    Drew crash onto the screen for the first time

--- src/entities/crash.js
+++ src/entities/crash.js
@@ -3,10 +3,10 @@ class Crash {
         this.game = game;
 
         this.animations = [];
-        this.sprite = ASSET_MANAGER.getAsset("./res/img/player.png");
+        this.sprite = ASSET_MANAGER.getAsset("./assets/images/crash_spritesheet.png");
 
-        this.facing = 0; // 0 = forward, 1 = right, 2 = backwards, 3 = left
-        this.state = 1; // 0 = idle, 1 = walking
+        this.facing = 0; // 0 = left, 1 = right
+        this.state = 0; // 0 = idle, 1 = running, 2 = jumping
 
         this.x = 100;
         this.y = 100;
@@ -19,53 +19,48 @@ class Crash {
     loadAnimations() {
         // Animator(this.sprite, x, y, width, height, framesCount, duration, padding, reverse, loop));
 
-        for (let i = 0; i < 4; i++) { // four directions
+        for (let i = 0; i < 2; i++) { // two directions
             this.animations.push([]);
-            for (let j = 0; j < 2; j++) { // two states
+            for (let j = 0; j < 3; j++) { // three states
                 this.animations[i].push([]);
             }
         }
 
-        // forward
-        this.animations[0][0] = new Animator(this.sprite, 14, 0, 14, 21, 1, 0.15, 0, false, true); // idle
-        this.animations[0][1] = new Animator(this.sprite, 0, 0, 14, 21, 4, 0.15, 0, false, true); // walking
-
-        // right
-        this.animations[1][0] = new Animator(this.sprite, 14, 44, 14, 21, 1, 0.15, 0, false, true); // idle
-        this.animations[1][1] = new Animator(this.sprite, 0, 44, 14, 21, 4, 0.15, 0, false, true); // walking
-
-        // backwards
-        this.animations[2][0] = new Animator(this.sprite, 14, 22, 14, 21, 1, 0.15, 0, false, true); // idle
-        this.animations[2][1] = new Animator(this.sprite, 0, 22, 14, 21, 4, 0.15, 0, false, true); // walking
-
+        // idle left -> 4 frames
+        // starts at (264, 114)
+        // w: 48, h: 65
         // left
-        this.animations[3][0] = new Animator(this.sprite, 14, 66, 14, 21, 1, 0.15, 0, false, true); // idle
-        this.animations[3][1] = new Animator(this.sprite, 0, 66, 14, 21, 4, 0.15, 0, false, true); // walking
+        this.animations[0][0] = new Animator(this.sprite, 264, 114, 51, 65, 1, 0.15, 0, false, true); // idle
+        // this.animations[3][1] = new Animator(this.sprite, 0, 66, 14, 21, 4, 0.15, 0, false, true); // running
+
+        // // right
+        // this.animations[1][0] = new Animator(this.sprite, 14, 44, 14, 21, 1, 0.15, 0, false, true); // idle
+        // this.animations[1][1] = new Animator(this.sprite, 0, 44, 14, 21, 4, 0.15, 0, false, true); // running
     };
 
     update() {
 
-        const WALK_SPEED = 100;
+        // const WALK_SPEED = 100;
 
-        if (this.game.keys["ArrowDown"] || this.game.keys["s"]) {
-            this.y += this.velocity;
-            this.facing = 0;
-            this.state = 1;
-        } else if (this.game.keys["ArrowRight"] || this.game.keys["d"]) {
-            this.x += this.velocity;
-            this.facing = 1;
-            this.state = 1;
-        } else if (this.game.keys["ArrowUp"] || this.game.keys["w"]) {
-            this.y -= this.velocity;
-            this.facing = 2;
-            this.state = 1;
-        } else if (this.game.keys["ArrowLeft"] || this.game.keys["a"]) {
-            this.x -= this.velocity;
-            this.facing = 3;
-            this.state = 1;
-        } else {
-            this.state = 0;
-        }
+        // if (this.game.keys["ArrowDown"] || this.game.keys["s"]) {
+        //     this.y += this.velocity;
+        //     this.facing = 0;
+        //     this.state = 1;
+        // } else if (this.game.keys["ArrowRight"] || this.game.keys["d"]) {
+        //     this.x += this.velocity;
+        //     this.facing = 1;
+        //     this.state = 1;
+        // } else if (this.game.keys["ArrowUp"] || this.game.keys["w"]) {
+        //     this.y -= this.velocity;
+        //     this.facing = 2;
+        //     this.state = 1;
+        // } else if (this.game.keys["ArrowLeft"] || this.game.keys["a"]) {
+        //     this.x -= this.velocity;
+        //     this.facing = 3;
+        //     this.state = 1;
+        // } else {
+        //     this.state = 0;
+        // }
         
     };
     
