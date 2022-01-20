00bebc792261afd0dc162548017c200d9f046596
alikb <alikb@uw.edu>
2022-01-20 13:46:26 -0800

    Added crash_spritesheet to the asset manager, added image smoothing to make crash more clear and added the crash entity to the screen

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
+ASSET_MANAGER.queueDownload("./assets/images/crash_spritesheet.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+
+	gameEngine.addEntity(new Crash(gameEngine));
 
 	gameEngine.init(ctx);
 
