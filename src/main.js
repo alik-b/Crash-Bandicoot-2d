21a836beefa3031162cb3b7320003c90c470047c
alikb <alikb@uw.edu>
2022-01-27 01:37:07 -0800

    added the specific entities required for the game

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./assets/images/crash_spritesheet.png");
+ASSET_MANAGER.queueDownload("./assets/images/Tiles/tiles.png");
+ASSET_MANAGER.queueDownload("./assets/images/BG/bg.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
@@ -9,10 +11,22 @@ ASSET_MANAGER.downloadAll(() => {
 	ctx.imageSmoothingEnabled = false;
 
 	canvas.width = innerWidth;
-	canvas.height = innerHeight;
+	canvas.height = 780
 
 	gameEngine.addEntity(new Crash(gameEngine));
-	gameEngine.addEntity(new Platform(gameEngine));
+	start = -1000;
+	for (i = 0; i < 200; i++) {
+		gameEngine.addEntity(new Platform(gameEngine, start, 600, 0));
+		start += 128;
+	}
+	gameEngine.addEntity(new Platform(gameEngine, 500, 200, 1));
+	gameEngine.addEntity(new Platform(gameEngine, 500+128, 200, 1));
+	gameEngine.addEntity(new Platform(gameEngine, 500+128*2, 200, 1));
+
+	gameEngine.addEntity(new Background(gameEngine, 0, 0));
+	gameEngine.addEntity(new Background(gameEngine, 320, 0));
+	gameEngine.addEntity(new Background(gameEngine, 320*2, 0));
+	gameEngine.addEntity(new Background(gameEngine, 320*3, 0));
 
 	gameEngine.init(ctx);
 
