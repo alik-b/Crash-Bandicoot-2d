9c8107e1ad47f229b21549a256227fe2421868e3
alikb <alikb@uw.edu>
2022-02-04 12:28:46 -0800

    added walls into the game

--- src/engine/scenemanager.js
+++ src/engine/scenemanager.js
@@ -21,29 +21,50 @@ class SceneManager {
     addAllEntities() {
         this.game.addEntity(this.crash);
         let start = -1000;
+
+        this.addnewWall(-411, 472, 4, 2, 127);
+        this.addnewWall(-539, 472, 4, 1, 127);
+        this.addnewWall(-667, 472, 4, 1, 127);
+        this.addnewWall(-795, 472, 4, 1, 127);
+        this.addnewWall(-923, 472, 4, 1, 127);
+        this.addnewWall(-1051, 472, 4, 1, 127);
+        //this.game.addEntity(new Wall(this.game, -410, 39, 3));
+
         for (let i = 0; i < 200; i++) {
             this.game.addEntity(new Platform(this.game, start, 600, 0));
             start += 128;
         }
-        this.game.addEntity(new Platform(this.game, 500, 200, 1));
-        this.game.addEntity(new Platform(this.game, 500+128, 200, 1));
-        this.game.addEntity(new Platform(this.game, 500+128*2, 200, 1));
+        this.addNewPlatform(500, 200, 3);
 
         for (let i = 0; i < this.canvasWidth / 320; i++) {
             this.game.addEntity(new Background(this.game, 320*i, 0));		
         }
     };
 
+    addNewPlatform(x, y, length) {
+        for (let i = 0; i < length; i++) {
+            this.game.addEntity(new Platform(this.game, x + 128*i, y, 1));
+        }
+    }
+
+    addnewWall(x, y, height, type, wall_height) {
+        let i;
+        for (i = 0; i < height; i++) {
+            this.game.addEntity(new Wall(this.game, x, y-wall_height*i, type));
+        }
+        this.game.addEntity(new Wall(this.game, x, y-wall_height*(i - 1) - 52, type + 2));
+    }
+
     update() {
         const PLAYER_WIDTH = 53;
         let midx = this.canvasWidth/2 - PLAYER_WIDTH/2;
-
-        this.x = this.crash.x - midx;
+        this.x = this.crash.position.x - midx;
     };
 
     draw(ctx) {
-        
     };
+
+
 
 
 }
