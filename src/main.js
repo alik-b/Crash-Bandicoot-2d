b6139b24c50d0c124810c29dc229f175d7f427d4
alikb <alikb@uw.edu>
2022-02-04 10:23:36 -0800

    Refactored main. Added scene manager into main

--- src/main.js
+++ src/main.js
@@ -6,28 +6,14 @@ ASSET_MANAGER.queueDownload("./assets/images/Tiles/tiles.png");
 ASSET_MANAGER.queueDownload("./assets/images/BG/bg.png");
 
 ASSET_MANAGER.downloadAll(() => {
-	const canvas = document.getElementById("gameWorld");
+	var canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
 
 	canvas.width = innerWidth;
 	canvas.height = 780;
-	console.log("width: " + canvas.width);
 
-	gameEngine.addEntity(new Crash(gameEngine));
-	start = -1000;
-	for (i = 0; i < 200; i++) {
-		gameEngine.addEntity(new Platform(gameEngine, start, 600, 0));
-		start += 128;
-	}
-	gameEngine.addEntity(new Platform(gameEngine, 500, 200, 1));
-	gameEngine.addEntity(new Platform(gameEngine, 500+128, 200, 1));
-	gameEngine.addEntity(new Platform(gameEngine, 500+128*2, 200, 1));
-
-	i = 0;
-	for (i = 0; i < canvas.width / 320; i++) {
-		gameEngine.addEntity(new Background(gameEngine, 320*i, 0));		
-	}
+	gameEngine.addEntity(new SceneManager(gameEngine, canvas.width));
 	gameEngine.init(ctx);
 
 	gameEngine.start();
